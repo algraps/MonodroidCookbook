8c0a56882bf2cd7546e9bc0be194632e7cbe9904
Alessandro Graps <alessandro.graps@gmail.com>
2013-07-02 01:04:53 +0200

    Contacts List App

---
+++ ContactList/code/Activity1.cs
@@ -0,0 +1,58 @@
+﻿/*
+ * Contacts App
+ * Author: Alessandro Graps
+ * Year: 2013
+ */
+using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Database;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Provider;
+using Uri = Android.Net.Uri;
+namespace Contacts
+{
+    [Activity(Label = "AndroidApplication1", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        private ListView _list;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            _list = FindViewById<ListView>(Resource.Id.contact_list);
+
+            var contacts = ManagedQuery(ContactsContract.Contacts.ContentUri, null, null, null, null);
+
+            _list.Adapter =
+                new SimpleCursorAdapter(
+                    this,
+                    Resource.Layout.item,
+                    contacts,
+                    new string[] { ContactsContract.ContactsColumnsConsts.DisplayName },
+                    new int[] { Resource.Id.contact_name });
+            _list.ItemClick += new EventHandler<AdapterView.ItemClickEventArgs>(_list_ItemClick);
+        }
+
+        private void _list_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            var item = (ICursor)_list.Adapter.GetItem(e.Position);
+            int id = item.GetInt(item.GetColumnIndex("_id"));
+            var intent = new Intent(Intent.ActionView);
+            var uri = Uri.WithAppendedPath(ContactsContract.Contacts.ContentUri, id.ToString());
+
+            intent.SetData(uri);
+            StartActivity(intent);
+        }
+
+    }
+}
+
---
+++ ContactList/code/PickContactActivity.cs
@@ -0,0 +1,56 @@
+/*
+ * Contacts App
+ * Author: Alessandro Graps
+ * Year: 2013
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Contacts
+{
+    [Activity(Label = "My Activity")]
+    public class PickContactActivity : Activity
+    {
+        private const int PICK_CONTACT = 42;
+        private Button _button;
+        private TextView _contactName;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.pick_contact);
+
+            _button = FindViewById<Button>(Resource.Id.button);
+            _contactName = FindViewById<TextView>(Resource.Id.contact_name);
+
+            _button.Click += delegate
+            {
+                var intent = new Intent(Intent.ActionPick, ContactsContract.Contacts.ContentUri);
+                StartActivityForResult(intent, PICK_CONTACT);
+            };
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if (requestCode == PICK_CONTACT)
+            {
+                string id = data.Data.LastPathSegment;
+                var contacts = ManagedQuery(ContactsContract.Contacts.ContentUri, null, "_id = ?", new string[] { id }, null);
+                contacts.MoveToFirst();
+
+                _contactName.Text = "Got contact: " + contacts.GetString(contacts.GetColumnIndex("display_name"));
+            }
+        }
+    }
+}
