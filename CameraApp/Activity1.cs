aea80ae52d74c8e3fd3bfda8d981dea1e5e305c0
Alessandro Graps <alessandro.graps@gmail.com>
2013-07-11 00:55:37 +0200

    CameraApp

---
+++ CameraApp/Activity1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Graphics;
+using Android.Provider;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Java.IO;
+using Environment = Android.OS.Environment;
+
+
+namespace CameraApp
+{
+    [Activity(Label = "CameraApp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class Activity1 : Activity
+    {
+        Java.IO.File _file;
+        Java.IO.File _dir;
+        ImageView _imageView;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            if (CheckAppToTakePictures())
+            {
+                CreateDirectoryForPictures();
+
+                Button button = FindViewById<Button>(Resource.Id.myButton);
+                _imageView = FindViewById<ImageView>(Resource.Id.imageView1);
+
+                button.Click += GetAPicture;
+            }
+        }
+
+        private bool CheckAppToTakePictures()
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            IList<ResolveInfo> availableActivities = PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private void CreateDirectoryForPictures()
+        {
+            _dir = new File(Environment.GetExternalStoragePublicDirectory(Environment.DirectoryPictures), "CameraApp");
+            if (!_dir.Exists())
+            {
+                _dir.Mkdirs();
+            }
+        }
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+
+            // make it available in the gallery
+            Intent mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            Android.Net.Uri contentUri = Android.Net.Uri.FromFile(_file);
+            mediaScanIntent.SetData(contentUri);
+            SendBroadcast(mediaScanIntent);
+
+            // display in ImageView. We will resize the bitmap to fit the display
+            // Loading the full sized image will consume to much memory 
+            // and cause the application to crash.
+            int height = _imageView.Height;
+            int width = Resources.DisplayMetrics.WidthPixels;
+            using (Bitmap bitmap = _file.Path.LoadAndResizeBitmap(width, height))
+            {
+                _imageView.SetImageBitmap(bitmap);
+            }
+        }
+
+        private void GetAPicture(object sender, EventArgs eventArgs)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+
+            _file = new File(_dir, String.Format("myPhoto_{0}.jpg", Guid.NewGuid()));
+
+            intent.PutExtra(MediaStore.ExtraOutput, Android.Net.Uri.FromFile(_file));
+
+            StartActivityForResult(intent, 0);
+        }
+    }
+}
+
